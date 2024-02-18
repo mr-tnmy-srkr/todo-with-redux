660244034dc5a778d52ad2372706b987b77b5551
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-02-18 20:27:50 +0530

    basic template and design created

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Tasks from '../pages/Tasks';
+import Chat from '../pages/Chat';
+import Settings from '../pages/Settings';
+import Profile from '../pages/Profile';
+
+const Routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Tasks />,
+      },
+      {
+        path: 'chat',
+        element: <Chat />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+export default Routes;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2453DF',
+        secondary: '#0C1F58',
+      },
+    },
+  },
+  plugins: [],
+};
